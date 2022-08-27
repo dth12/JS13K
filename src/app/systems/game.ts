a5e079dfc0939e354d79d4fb81af55c2e52bc0f9
dooyeong20 <noviced22@gmail.com>
2022-08-27 22:25:38 +0900

    add toggle flash light feature
    - toggle with space key

--- src/app/systems/game.ts
+++ src/app/systems/game.ts
@@ -5,6 +5,16 @@ AFRAME.registerSystem('game', {
 
   init() {
     console.log('Game Initialized');
+    // TODO: Move to seperate class (ex. Player)
+    document.addEventListener('keydown', (event) => {
+      switch (event.key) {
+        case ' ':
+          const $flash = document.querySelector('#flash');
+          const isFlashOn = $flash.getAttribute('intensity') !== '0';
+
+          $flash.setAttribute('intensity', isFlashOn ? '0' : '1 ');
+      }
+    });
     Level.createStage(1);
   },
 
