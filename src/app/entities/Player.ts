a9e3c707645c5aba1540de6d6e30911a7cdbb0a0
dooyeong20 <noviced22@gmail.com>
2022-08-28 16:17:59 +0900

    create player class

---
+++ src/app/entities/Player.ts
@@ -0,0 +1,47 @@
+import { Entity } from 'aframe';
+
+export class Player {
+  private $el: Entity;
+  private $flash: Entity;
+
+  private get isFlashOn() {
+    return this.$flash.getAttribute('intensity') !== '0';
+  }
+
+  constructor(playerOption: { [key: string]: any }, lightOption: { [key: string]: any }) {
+    const $gameScene = document.querySelector('#gameScene');
+
+    this.initFlash(lightOption);
+    this.initPlayer(playerOption);
+
+    this.$el.appendChild(this.$flash);
+    $gameScene.appendChild(this.$el);
+
+    this.initEventHandler();
+  }
+
+  private initFlash(option: { [key: string]: any }) {
+    this.$flash = document.createElement('a-light');
+    Object.keys(option).forEach((key) => {
+      this.$flash.setAttribute(key, option[key]);
+    });
+  }
+
+  private initPlayer(option: { [key: string]: any }) {
+    this.$el = document.createElement('a-entity');
+    this.$el.id = 'player';
+    Object.keys(option).forEach((key) => {
+      this.$el.setAttribute(key, option[key]);
+    });
+  }
+
+  private initEventHandler() {
+    document.addEventListener('keydown', (event) => {
+      if (event.key !== ' ') {
+        return;
+      }
+
+      this.$flash.setAttribute('intensity', this.isFlashOn ? '0' : '1 ');
+    });
+  }
+}
--- src/app/systems/game.ts
+++ src/app/systems/game.ts
@@ -1,23 +1,30 @@
+import { Player } from '../entities/Player';
 import { Level } from './level';
 
 AFRAME.registerSystem('game', {
   schema: {},
-
   init() {
     console.log('Game Initialized');
-    // TODO: Move to seperate class (ex. Player)
-    document.addEventListener('keydown', (event) => {
-      switch (event.key) {
-        case ' ':
-          const $flash = document.querySelector('#flash');
-          const isFlashOn = $flash.getAttribute('intensity') !== '0';
 
-          $flash.setAttribute('intensity', isFlashOn ? '0' : '1 ');
+    new Player(
+      {
+        camera: {},
+        'wasd-controls': {},
+        'look-controls': { pointerLockEnabled: true },
+        position: { a: 0, y: 2.5, z: 0 },
+      },
+      {
+        type: 'spot',
+        angle: '30',
+        decay: '1',
+        distance: '30',
+        color: '#fff',
+        intensity: '0',
+        position: '0 0 0',
       }
-    });
+    );
     Level.createStage(1);
   },
-
   tick(_time, timeDelta) {
     Level.update(timeDelta);
   },
