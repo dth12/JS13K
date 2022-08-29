066c0e5ea8ba8aa67f8419bf3d3c524c84fc783c
dooyeong20 <noviced22@gmail.com>
2022-08-29 19:58:45 +0900

    add running feature

--- src/app/entities/Player.ts
+++ src/app/entities/Player.ts
@@ -1,11 +1,15 @@
 import { Entity } from 'aframe';
+import { Light, Speed } from '../types';
 
 export class Player {
   private $el: Entity;
   private $flash: Entity;
+  private get isRunning() {
+    return this.$el.getAttribute('wasd-controls').acceleration === Speed.Run;
+  }
 
   private get isFlashOn() {
-    return this.$flash.getAttribute('intensity') !== '0';
+    return this.$flash.getAttribute('intensity') !== Light.Off;
   }
 
   constructor(playerOption: { [key: string]: any }, lightOption: { [key: string]: any }) {
@@ -37,11 +41,16 @@ export class Player {
 
   private initEventHandler() {
     document.addEventListener('keydown', (event) => {
-      if (event.key !== ' ') {
-        return;
+      switch (event.key) {
+        case ' ':
+          this.$flash.setAttribute('intensity', this.isFlashOn ? Light.Off : Light.On);
+          break;
+        case 'Control':
+          const config = this.$el.getAttribute('wasd-controls');
+          this.$el.setAttribute('wasd-controls', { ...config, acceleration: this.isRunning ? Speed.Walk : Speed.Run });
+          break;
+        default:
       }
-
-      this.$flash.setAttribute('intensity', this.isFlashOn ? '0' : '1');
     });
   }
 }
