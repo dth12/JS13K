9a1174eadbd36962f707743a49910c915276ce4d
jade.kjo <jade.kjo@kakaocorp.com>
2022-09-01 09:50:27 +0900

    randomize music

--- src/utils/music-generator.js
+++ src/utils/music-generator.js
@@ -1,27 +1,27 @@
 import { song } from "../sounds/song.js";
 
-export function startDemo() {
-    // Initialize music generation (player).
-    var player = new CPlayer();
-    player.init(song);
+export const generateMusic = () => {
+  // Initialize music generation (player).
+  const player = new CPlayer();
+  player.init(song);
+
+  // Generate music...
+  let done = false;
   
-    // Generate music...
-    var done = false;
-    setInterval(function () {
-      if (done) {
-        return;
-      }
-  
-      done = player.generate() >= 1;
-  
-      if (done) {
-  
-        // Put the generated song in an Audio element.
-        var wave = player.createWave();
-        var audio = document.createElement("audio");
-        audio.src = URL.createObjectURL(new Blob([wave], {type: "audio/wav"}));
-        audio.loop = true;
-        audio.play();
-      }
-    }, 0);
-  }
+  setInterval(function () {
+    if (done) {
+      return;
+    }
+
+    done = player.generate() >= 1;
+
+    if (done) {
+      // Put the generated song in an Audio element.
+      var wave = player.createWave();
+      var audio = document.createElement("audio");
+      audio.src = URL.createObjectURL(new Blob([wave], {type: "audio/wav"}));
+      audio.loop = true;
+      audio.play();
+    }
+  }, 0);
+}
