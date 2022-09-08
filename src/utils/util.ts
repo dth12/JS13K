5c53048231ea8308e4f7b06a1a7838f5544584a1
jade.kjo <jade.kjo@kakaocorp.com>
2022-09-09 00:39:27 +0900

    add constants and utils

--- src/utils/util.ts
+++ src/utils/util.ts
@@ -1,9 +1,27 @@
+import {MIN_NOTE, MAX_NOTE, MAX_MELODY_LENGTH} from '../app/constants/constant';
+
 // getRandomRange(10) => -10 < x < 10
-export const getRandomRange = (num: number) => {
+export const getRandomRange = (num: number): number => {
   return Math.random() * num * 2 - num;
 };
 
 // getRandomIndex(3) => 0 <= x < 3
-export const getRandomIndex = (num: number) => {
+export const getRandomIndex = (num: number): number => {
   return Math.floor(Math.random() * num);
 };
+
+// getRandomNote() => MIN_NOTE <= x < MAX_NOTE
+export const getRandomNote = (): number => {
+  return Math.floor(Math.random() * (MAX_NOTE - MIN_NOTE)) + MIN_NOTE; 
+}
+
+// getRandomMelody() => songData.c.n
+export const getRandomMelody = (): number[] => {
+  const randomMelody: number[] = [];
+
+  for (let i: number = 0; i < MAX_MELODY_LENGTH; ++i) {
+    randomMelody.push(...[getRandomNote(),,,,,,,,]);
+  }
+
+  return randomMelody;
+}
