5c4a8326a3886febfb3fa27686dda93e49d10bae
jade.kjo <jade.kjo@kakaocorp.com>
2022-09-10 18:54:42 +0900

    change muted schema to isMuted state

--- src/app/components/bgm.ts
+++ src/app/components/bgm.ts
@@ -2,7 +2,6 @@ AFRAME.registerComponent('bgm', {
   schema: {
     sequence: {default: 'soundBox'},
     volume: {default: 0.0},
-    muted: {default: false},
     autoplay: {default: false},
     loop: {default: false},
   },
