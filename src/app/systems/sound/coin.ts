2e8f84b3f4e5e73dc501d1ed3ceeffcbb79e08ed
jade.kjo <jade.kjo@kakaocorp.com>
2022-09-13 18:23:33 +0900

    fix playAudio and pauseAudio

--- src/app/systems/sound/coin.ts
+++ src/app/systems/sound/coin.ts
@@ -21,9 +21,13 @@ AFRAME.registerSystem('coin', {
     }
   },
   playAudio() {
-    this.audio.play();
+    if (!(this.audio.currentTime > 0 && !this.audio.paused && !this.audio.ended && this.audio.readyState > 2)) {
+      this.audio.play();
+    }
   },
   pauseAudio() {
-    this.audio.pause();
+    if (this.audio.currentTime > 0 && !this.audio.paused && !this.audio.ended && this.audio.readyState > 2) {
+      this.audio.pause();
+    }
   },
 });
